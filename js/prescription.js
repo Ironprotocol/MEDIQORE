79d7cda7ddc87e65e303c2c180efb4c38b059a07
IronProtocol <ironmasks90@gmail.com>
2025-02-18 23:34:38 +0200

    재정비
    재정비

--- js/prescription.js
+++ js/prescription.js
@@ -2,13 +2,15 @@
 export function initializePrescription() {
     const prescriptionTitle = document.querySelector('#prescription-content .content-title');
     const noPatientMessage = document.querySelector('#prescription-content .no-patient-message');
-    const prescriptionContent = document.querySelector('#prescription-content .prescription-content');
 
     // Room의 환자 클릭 이벤트에 대한 처리
     document.addEventListener('prescriptionPatientSelected', (e) => {
         const { name, age } = e.detail;
-        prescriptionTitle.textContent = `${name} Prescription`;
-        noPatientMessage.style.display = 'none';
-        prescriptionContent.style.display = 'flex';
+        if (prescriptionTitle) {
+            prescriptionTitle.textContent = `${name} Prescription`;
+        }
+        if (noPatientMessage) {
+            noPatientMessage.style.display = 'none';
+        }
     });
 }
