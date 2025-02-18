ee1b5e0afc0a836e18ce2ce73e7ad3a681640d79
IronProtocol <ironmasks90@gmail.com>
2025-02-18 19:57:45 +0200

    prescription 분기점
    prescription 분기점

--- js/prescription.js
+++ js/prescription.js
@@ -2,11 +2,13 @@
 export function initializePrescription() {
     const prescriptionTitle = document.querySelector('#prescription-content .content-title');
     const noPatientMessage = document.querySelector('#prescription-content .no-patient-message');
+    const prescriptionContent = document.querySelector('#prescription-content .prescription-content');
 
     // Room의 환자 클릭 이벤트에 대한 처리
     document.addEventListener('prescriptionPatientSelected', (e) => {
         const { name, age } = e.detail;
         prescriptionTitle.textContent = `${name} Prescription`;
         noPatientMessage.style.display = 'none';
+        prescriptionContent.style.display = 'flex';
     });
 }
