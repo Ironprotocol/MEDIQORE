eb887906db2f90ea9ff0dfe3829f63f1cfffd362
IronProtocol <ironmasks90@gmail.com>
2025-02-21 15:50:11 +0200

    prescription 레이아웃 수정
    prescription 레이아웃 수정

--- js/prescription.js
+++ js/prescription.js
@@ -2,7 +2,7 @@
 export function initializePrescription() {
     const prescriptionTitle = document.querySelector('#prescription-content .content-title-prescription');
     const noPatientMessage = document.querySelector('#prescription-content .no-patient-message');
-    const contentBody = document.querySelector('#prescription-content .content-body-prescription');
+    const prescriptionLayout = document.querySelector('#prescription-content .prescription-layout');
 
     // Room의 환자 클릭 이벤트에 대한 처리
     document.addEventListener('prescriptionPatientSelected', (e) => {
@@ -10,8 +10,9 @@ export function initializePrescription() {
         if (prescriptionTitle) {
             prescriptionTitle.textContent = `${name} Prescription`;
         }
-        if (noPatientMessage) {
+        if (noPatientMessage && prescriptionLayout) {
             noPatientMessage.style.display = 'none';
+            prescriptionLayout.style.display = 'flex';
         }
     });
 }
